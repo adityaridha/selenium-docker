4a1b0a9042b848c1c8536679424fe135dd29de9f
adityaridha <adityaridharrahman@gmail.com>
2020-07-15 12:05:00 +0700

    dockerize test

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,12 @@
 FROM python:3
 
-ADD test_homepage.py /
+ENV REM_DRIVER="http://0.0.0.0:4444/wd/hub"
 
-CMD [ "python", "./test_homepage.py"]
+RUN mkdir /tests
+COPY . /tests
+
+WORKDIR /tests
+
+RUN pip install -r requirements.txt
+
+CMD [ "python", "test_homepage.py"]
--- test_homepage.py
+++ test_homepage.py
@@ -1,6 +1,12 @@
 from selenium import webdriver
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+import os
 
-driver = webdriver.Chrome()
+remote_driver = os.environ['REM_DRIVER']
+
+capabilities = {}
+capabilities = DesiredCapabilities.CHROME.copy()
+driver = webdriver.Remote(command_executor=remote_driver, desired_capabilities=capabilities)
 driver.get("https://fresh.efishery.com")
 
 # verify able to select city
